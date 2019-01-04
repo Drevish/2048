30a134f8cf61ff1d392b0b389bb5328526d6ea12
Vladimir Nemolovsky <vladimirnemolovsky@gmail.com>
2019-01-04 23:11:24 +0200

    code improvements

--- script.js
+++ script.js
@@ -165,13 +165,16 @@ class Board {
 		this.emptyCells.delete(i, j);
 	}
 
-	moveMap(direction){
-		// copy this.map to the map
-		let map = this.map
-
+	// makes a move in a specified direction and returns true if the map was changed and false otherwise
+	moveMap(direction) {
 		// flag that is true if at least one tile has changed it's location
 		let somethingChanged = false;
 
+		/*
+		 * array that stores information about tiles that was changed 
+		 * we need it because by the rules we can't change the same tile twice so that 2222 
+		 * can't be transformed to 8, we can transform it only to 44 
+		 */
 		let changedTiles = [];
 		for (let i = 0; i < this.mapSize; i++) {
 			changedTiles[i] = [];
@@ -180,167 +183,238 @@ class Board {
 			}
 		}
 
-		if(direction == Direction.UP) {
-			for (let j = 0; j < this.mapSize; j++) {
-				for (let i = 1; i < this.mapSize; i++) {
-					if (map[i][j] == 0) continue;
-					let newI = i;
-					let curVal = map[i][j];
-					for (let k = i - 1; k >= 0; k--) {
-						if (map[k][j] == 0) {
-							newI = k;
-							continue;
-						}
-						if (map[k][j] != curVal) break;
-						if (map[k][j] == curVal) {
-							newI = k; 
-							break;
-						}
-					}
-					if (newI == i) continue;
-					map[i][j] = 0;
-					somethingChanged = true;
-					this.emptyCells.addNew(i, j);
-					if (map[newI][j] == 0) {
-						map[newI][j] = curVal;
-						this.emptyCells.delete(newI, j);
-						continue;
-					}
-					if (map[newI][j] == curVal && changedTiles[newI][j] == true) {
-						map[newI + 1][j] = curVal;
-						this.emptyCells.delete(newI + 1, j);
-						continue;
-					}
-					if (map[newI][j] == curVal && changedTiles[newI][j] == false) {
-						map[newI][j] = curVal * 2;
-						changedTiles[newI][j] = true;
-						this.score += curVal * 2;
-					}
+
+		/*
+		 * loop 1 - loop through columns or rows
+		 * loop 2 - nested loop through tiles in a current column or row
+		 * loop 3 - third loop which is when the current tile is already choosed and we 
+		 *	 		have to watch the current column / row through to find the deadlock
+		 */
+
+		// true when we have to exit from this loop
+		let loop1Exit = false;
+		let loop2Exit = false;
+
+		// loops variables
+		let i = 0;
+		let j = 0;
+		let k = 0;
+
+		// needs for every iteration through first loop, iterates first loop variable
+		let loop1 = function(direction) {
+			switch(direction) {
+				case Direction.UP:
+				case Direction.DOWN:
+				// j from 0 to mapSize
+				if (j < this.mapSize - 1) {
+					j++;
+				} else {
+					loop1Exit = true;
 				}
+				break;
+
+				case Direction.LEFT:
+				case Direction.RIGHT:
+				// i from 0 to mapSize
+				if (i < this.mapSize - 1) {
+					i++;
+				} else {
+					loop1Exit = true;
+				}
+				break;
+			}
+		}.bind(this);
+
+		// needs for loop 2 variables initialization
+		let loop2Init = function(direction) {
+			switch(direction) {
+				case Direction.UP:
+				i = 1;
+				break;
+
+				case Direction.DOWN:
+				i = this.mapSize - 2;
+				break;
+
+				case Direction.LEFT:
+				j = 1;
+				break;
+
+				case Direction.RIGHT:
+				j = this.mapSize - 2;
+				break;
+			}
+		}.bind(this);
+
+		// needs for every iteration through first loop, iterates second loop variable
+		let loop2 = function(direction) {
+			switch(direction) {
+				case Direction.UP:
+				// i from 1 to mapSize 
+				if (i < this.mapSize - 1) {
+					i++;
+				} else {
+					loop2Exit = true;
+				}
+				break;
+
+				case Direction.DOWN:
+				// i from mapSize - 2 to 0 inclusive
+				if (i > 0) {
+					i--;
+				} else {
+					loop2Exit = true;
+				}
+				break;
+
+				case Direction.LEFT:
+				// j from 1 to mapSize
+				if (j < this.mapSize - 1) {
+					j++;
+				} else {
+					loop2Exit = true;
+				}
+				break; 
+
+				case Direction.RIGHT:
+				// j from mapSize - 2 to 0 inclusive
+				if (j > 0) {
+					j--;
+				} else {
+					loop2Exit = true;
+				}
+				break;
+			}
+		}.bind(this);
+
+		// needs for loop 2 variable initialization
+		let loop3Init = function(direction) {
+			switch(direction) {
+				case Direction.UP: 
+				case Direction.DOWN:
+				k = i;
+				break;
+
+				case Direction.LEFT: 
+				case Direction.RIGHT:
+				k = j;
+				break;
+			}
+		}.bind(this);
+
+		// needs for every iteration through third loop, iterates k and
+		// returns two values which are indexes for current tile looking for 
+		let loop3 = function(direction) {
+			switch(direction) {
+				case Direction.UP:
+				k--;
+				return new Cell(k, j);
+
+				case Direction.DOWN:
+				k++;
+				return new Cell(k, j);
+				
+				case Direction.LEFT:
+				k--;
+				return new Cell(i, k);
+				
+				case Direction.RIGHT:
+				k++;
+				return new Cell(i, k);
 			}
 		}
 
+		// main loop
+		while (!loop1Exit) {
+			
+			loop2Init(direction);
+			while (!loop2Exit) {
+				// loop code
 
-		if(direction == Direction.DOWN) {
-			for (let j = 0; j < this.mapSize; j++) {
-				for (let i = this.mapSize - 2; i >= 0; i--) {
-					if (map[i][j] == 0) continue;
-					let newI = i;
-					let curVal = map[i][j];
-					for (let k = i + 1; k < this.mapSize; k++) {
-						if (map[k][j] == 0) {
-							newI = k;
-							continue;
-						}
-						if (map[k][j] != curVal) break;
-						if (map[k][j] == curVal) {
-							newI = k; 
-							break;
-						}
-					}
-					if (newI == i) continue;
-					map[i][j] = 0;
-					somethingChanged = true;
-					this.emptyCells.addNew(i, j);
-					if (map[newI][j] == 0) {
-						map[newI][j] = curVal;
-						this.emptyCells.delete(newI, j);
+				// empty tile, can't be transfered
+				if (this.map[i][j] == 0) {					
+					loop2(direction);
+					continue;
+				} 
+
+
+				// current tile that may be transfered
+				let currentValue = this.map[i][j];
+
+				// tile where we are going to place the current one
+				let newTile = new Cell(i, j);
+				loop3Init(direction);
+				while (true) {
+					// tile that we are currently looking at in the loop represented as map coordinates
+					let loopTile = loop3(direction);
+
+					// out of range
+					if (k < 0 || k >= this.mapSize) break;
+
+					// tile that we are currently looking at value
+					let loopTileValue = this.map[loopTile.i][loopTile.j];
+
+
+					// empty loop, current can be transfered here
+					if (loopTileValue == 0) {
+						newTile = loopTile;
 						continue;
 					}
-					if (map[newI][j] == curVal && changedTiles[newI][j] == true) {
-						map[newI - 1][j] = curVal;
-						this.emptyCells.delete(newI - 1, j);
-						continue;
+
+					// different tile, search stops
+					if (loopTileValue != currentValue) {
+						break;
 					}
-					if (map[newI][j] == curVal && changedTiles[newI][j] == false) {
-						map[newI][j] = curVal * 2;
-						changedTiles[newI][j] = true;
-						this.score += curVal * 2;
+
+					// the same tile, we should transfer the current one there so the search stops
+					if (loopTileValue == currentValue && changedTiles[loopTile.i][loopTile.j] == false) {
+						newTile = loopTile;
+						break;
+					}
+
+					// the same tile but it was already changed so we can't use it and have to use the previous one 
+					if (loopTileValue == currentValue && changedTiles[loopTile.i][loopTile.j] == true) {
+						break;
 					}
 				}
-			}
-		}
 
-
-		if(direction == Direction.LEFT) {
-			for (let i = 0; i< this.mapSize; i++) {
-				for (let j = 1; j < this.mapSize; j++) {
-					if (map[i][j] == 0) continue;
-					let newJ = j;
-					let curVal = map[i][j];
-					for (let k = j - 1; k >= 0; k--) {
-						if (map[i][k] == 0) {
-							newJ = k;
-							continue;
-						}
-						if (map[i][k] != curVal) break;
-						if (map[i][k] == curVal) {
-							newJ = k; 
-							break;
-						}
-					}
-					if (newJ == j) continue;
-					map[i][j] = 0;
-					somethingChanged = true;
-					this.emptyCells.addNew(i, j);
-					if (map[i][newJ] == 0) {
-						map[i][newJ] = curVal;
-						this.emptyCells.delete(i, newJ);
-						continue;
-					}
-					if (map[i][newJ] == curVal && changedTiles[i][newJ] == true) {
-						map[i][newJ + 1] = curVal;
-						this.emptyCells.delete(i, newJ + 1);
-						continue;
-					}
-					if (map[i][newJ] == curVal && changedTiles[i][newJ] == false) {
-						map[i][newJ] = curVal * 2;
-						changedTiles[i][newJ] = true;
-						this.score += curVal * 2;
-					}
+				// we are in deadlock, can't transfer current tile
+				if (newTile.i == i && newTile.j == j) {
+					loop2(direction);
+					continue;
 				}
-			}
-		}
 
+				// erasing current value in map 
+				this.map[i][j] = 0;
+				this.emptyCells.addNew(i, j);
+				somethingChanged = true;
 
-		if(direction == Direction.RIGHT) {
-			for (let i = 0; i< this.mapSize; i++) {
-				for (let j = this.mapSize - 2; j >= 0; j--) {
-					if (map[i][j] == 0) continue;
-					let newJ = j;
-					let curVal = map[i][j];
-					for (let k = j + 1; k < this.mapSize; k++) {
-						if (map[i][k] == 0) {
-							newJ = k;
-							continue;
-						}
-						if (map[i][k] != curVal) break;
-						if (map[i][k] == curVal) {
-							newJ = k; 
-							break;
-						}
-					}
-					if (newJ == j) continue;
-					map[i][j] = 0;
-					somethingChanged = true;
-					this.emptyCells.addNew(i, j);
-					if (map[i][newJ] == 0) {
-						map[i][newJ] = curVal;
-						this.emptyCells.delete(i, newJ);
-						continue;
-					}
-					if (map[i][newJ] == curVal && changedTiles[i][newJ] == true) {
-						map[i][newJ - 1] = curVal;
-						this.emptyCells.delete(i, newJ - 1);
-						continue;
-					}
-					if (map[i][newJ] == curVal && changedTiles[i][newJ] == false) {
-						map[i][newJ] = curVal * 2;
-						changedTiles[i][newJ] = true;
-						this.score += curVal * 2;
-					}
+				let newTileValue =this.map[newTile.i][newTile.j];
+
+				// simply inserting current tile if insertion place is empty
+				if (newTileValue == 0) {
+					this.map[newTile.i][newTile.j] = currentValue;
+					this.emptyCells.delete(newTile.i, newTile.j);
+					
+					loop2(direction);
+					continue;
 				}
+
+				// doubling new tile value, increasing score, marking new tile as changed
+				if (newTileValue == currentValue) {
+					this.map[newTile.i][newTile.j] = currentValue * 2;
+					changedTiles[newTile.i][newTile.j] = true;
+					this.score += currentValue * 2;
+
+					loop2(direction);
+					continue;
+				}
+
+				loop2(direction);
 			}
+			loop2Exit = false;
+
+			loop1(direction);
 		}
 
 		return somethingChanged;
@@ -356,8 +430,7 @@ class Board {
 }
 
 
-// let canvas = document.getElementById('gameCanvas');
-let drawer = new Drawer(/*canvas, CANVAS_SIZE*/);
+let drawer = new Drawer();
 let board;
 let gameStarted
 
